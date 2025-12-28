412ab7b5bccef376b15d5ebab4cbed718754b55f
hassansaif-0 <hassansaifraja102@gmail.com>
2025-12-28 21:12:13 +0500

    feat: added linklost node and stack

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <map>
+#include <cctype>
+#include <ctime>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+// ==================== LINKED LIST NODE ====================
+template<typename T>
+class ListNode{
+public:
+    T data;
+    ListNode* next;
+    
+    ListNode(T value) {
+        data = value;      
+        next = NULL;  
+    }
+};
+
+// ==================== LINKED LIST STACK ====================
+template<typename T>
+class LinkedListStack{
+    ListNode<T>* top;
+    int size;
+
+public:
+    LinkedListStack(){
+        top = NULL;
+        size = 0;
+    }
+
+
+    ~LinkedListStack(){
+        while (!isEmpty()){
+            pop();
+        }
+    }
+
+    void push(T item){
+        ListNode<T>* newNode = new ListNode<T>(item);
+        newNode->next = top;
+        top = newNode;
+        size++;
+    }
+
+    T pop(){
+        if (isEmpty()) {
+            return T();
+        }
+        
+        ListNode<T>* temp = top;
+        T item = top->data;
+        top = top->next;
+        delete temp;
+        size--;
+        
+        return item;
+    }
+
+    bool isEmpty() {
+        return top == NULL;
+    }
+
+    T peek() {
+        if (isEmpty()) {
+            return T();
+        }
+        return top->data;
+    }
+
+    int getSize() {
+        return size;
+    }
+};
